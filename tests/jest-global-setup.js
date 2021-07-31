4fd82283be3c20d29a72bee3904c85ab3b5350bc
AlexRogalskiy <a.rogalskii@bftcom.com>
2021-07-31 21:04:06 +0300

    Added info on workflows
    Updates on github-actions

--- tests/jest-global-setup.js
+++ tests/jest-global-setup.js
@@ -1,1 +1,13 @@
+// import fetchMock from "jest-fetch-mock"
+
+// rewire global.fetch to call 'fetchMock'
+// fetchMock.enableMocks();
+
+// Mock __non_webpack_require__ for tests
+// globalThis.__non_webpack_require__ = jest.fn();
+
+process.on("unhandledRejection", (err) => {
+    fail(err);
+});
+
 module.exports = async () => console.log(">>> JEST global setup >>>");
