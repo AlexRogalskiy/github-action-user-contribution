fd984c29f374d73791bad958f786fa94087c2654
Alexander Rogalskiy <arogalskii@sbereducation.ru>
2021-10-16 18:01:03 +0300

    Updates on files

--- tests/setup-jest.js
+++ tests/setup-jest.js
@@ -1,3 +1,11 @@
+import * as log from 'loglevel';
+
+if (process.env.DEBUG) {
+    log.setLevel('debug');
+} else {
+    log.disableAll();
+}
+
 global.__NODE_ENV__ = 'test'
 global.fetch = () => {
     console.log('fetch is mocked')
