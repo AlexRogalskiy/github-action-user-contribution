5876e95d62b524c2050f7f5e1b84e338a845e0dd
Alexander Rogalskiy <arogalskii@sbereducation.ru>
2021-11-26 13:01:44 +0300

    Updates on tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
 const path = require('path');
 
 const ROOT = path.resolve(__dirname);
